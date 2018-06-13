0adf8f7ee959e48245ec82284dad96fa82d4c13f
ro-fdm <rocio.fdez.diaz.meco@gmail.com>
2018-06-13 11:56:34 +0200

    errors_in_view

--- spec/web/controllers/books/new_spec.rb
+++ spec/web/controllers/books/new_spec.rb
@@ -1,11 +1,15 @@
 require_relative '../../../spec_helper'
 
 describe Web::Controllers::Books::New do
-  let(:action) { Web::Controllers::Books::New.new }
-  let(:params) { Hash[] }
+  let(:params)    { OpenStruct.new(valid?: false, error_messages: ['Title must be filled', 'Author must be filled']) }
+  let(:exposures) { Hash[params: params] }
+  let(:template)  { Hanami::View::Template.new('apps/web/templates/books/new.html.erb') }
+  let(:view)      { Web::Views::Books::New.new(template, exposures) }
+  let(:rendered)  { view.render }
 
-  it 'is successful' do
-    response = action.call(params)
-    response[0].must_equal 200
+  it 'displays list of errors when params contains errors' do
+    rendered.must_include('There was a problem with your submission')
+    rendered.must_include('Title must be filled')
+    rendered.must_include('Author must be filled')
   end
 end
